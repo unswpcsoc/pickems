de54905e11b762ec78bc60364dd334bc7a3dcfc6
Alex Takumi Gao <alex.takumi.gao1@gmail.com>
2025-02-05 15:08:33 +1100

    Corrected styling to twitch embed to work better on multiple resolutions

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,14 +6,14 @@ const HomePage = () => {
 
       {/* Twitch embedded player*/}
       <div style={{ marginBottom: '24px', display: 'flex', justifyContent: 'center', marginRight: '16px', color: 'white' }}>
-        {/* <iframe
-          src="https://player.twitch.tv/?channel=unswpcsoc&parent=localhost"
-          height="800"
-          width="100%"
+        <iframe
+          src="https://player.twitch.tv/?channel=unswpcsoc&parent=localhost&parent=alexgao.au"
+          height="100%"
+          width="95vw"
           title="Faker Stream"
           frameBorder="0"
           allowFullScreen={true}
-        ></iframe> */}
+        ></iframe>
       </div>
 
     </div>
